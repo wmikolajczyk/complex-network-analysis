3732ef7fb86a3412af58dafa074cc65b309c7bb3
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2017-12-07 18:41:35 +0100

    Create priority_rank package

---
+++ priority_rank/__main__.py
@@ -0,0 +1,14 @@
+# Load network 1 dataset
+
+# Load network 2 dataset
+
+# Transform network to training dataset
+
+# Priority Rank
+"""
+for each vertex n
+    compute ranking R (m-lenght)
+    for number of edges for vertex
+        sample vertex t from the ranking
+        add edge vertex - vertex t
+"""
