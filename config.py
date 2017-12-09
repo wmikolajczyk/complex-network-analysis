71fd7094102e24f79b8ea8a29793a9c9a5ed20ee
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2017-12-09 18:33:24 +0100

    Move file paths to config file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# File paths
+primaryschool_dataset_dir = 'Datasets/primary_school/'
+primaryschool_dataset = os.path.join(primaryschool_dataset_dir, 'primaryschool.csv')
+
+primaryschool_prepared_dataset = os.path.join(primaryschool_dataset_dir, 'primaryschool_prepared.csv')
+
+primaryschool_metadata = os.path.join(primaryschool_dataset_dir, 'metadata_primaryschool.txt')
--- priority_rank/__main__.py
+++ priority_rank/__main__.py
@@ -2,20 +2,14 @@ import networkx as nx
 import os
 
 from utils import prepare_primaryschool_dataset, read_primaryschool_metadata
+from config import primaryschool_prepared_dataset, primaryschool_dataset, primaryschool_metadata
+
 # Load network 1 dataset
-
-primaryschool_dataset_dir = 'Datasets/primary_school/'
-primaryschool_dataset = os.path.join(primaryschool_dataset_dir, 'primaryschool.csv')
-
 # Prepare dataset file
-primaryschool_prepared_dataset = os.path.join(primaryschool_dataset_dir, 'primaryschool_prepared.csv')
-
 if not os.path.exists(primaryschool_prepared_dataset):
     prepare_primaryschool_dataset(primaryschool_dataset, primaryschool_prepared_dataset)
 
 # Read metadata
-primaryschool_metadata = os.path.join(primaryschool_dataset_dir, 'metadata_primaryschool.txt')
-
 class_id, gender = read_primaryschool_metadata(primaryschool_metadata)
 
 # Create graph with edges loaded from dataset
@@ -25,6 +19,8 @@ nx.set_node_attributes(graph, class_id, 'class')
 nx.set_node_attributes(graph, gender, 'gender')
 
 print(graph.nodes(data=True))
+
+
 # Load network 2 dataset
 
 # Transform network to training dataset
