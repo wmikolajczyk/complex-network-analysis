aacbaab063cb22354766adc0c423a0871ce5f008
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2017-12-10 00:31:12 +0100

    Refactor workplace dataset generate csv

--- priority_rank/__main__.py
+++ priority_rank/__main__.py
@@ -3,17 +3,16 @@ import networkx as nx
 import os
 import pandas as pd
 
-from utils import PrimarySchoolDatasetHandler
+from utils import PrimarySchoolDatasetHandler, WorkplaceDatasetHandler
 from config import primaryschool, primaryschool_dataset_dir
 
 # Load Primary School dataset
-primary_school_handler = PrimarySchoolDatasetHandler()
 # Prepare dataset file
 if not os.path.exists(primaryschool['prepared_dataset']):
-    primary_school_handler.prepare_dataset(primaryschool['dataset'], primaryschool['prepared_dataset'])
+    PrimarySchoolDatasetHandler.prepare_dataset(primaryschool['dataset'], primaryschool['prepared_dataset'])
 
 # Read metadata
-class_id, gender = primary_school_handler.read_metadata(primaryschool['metadata'])
+class_id, gender = PrimarySchoolDatasetHandler.read_metadata(primaryschool['metadata'])
 
 # Create graph with edges loaded from dataset
 graph = nx.read_edgelist(primaryschool['prepared_dataset'], nodetype=int)
@@ -21,48 +20,22 @@ graph = nx.read_edgelist(primaryschool['prepared_dataset'], nodetype=int)
 nx.set_node_attributes(graph, class_id, 'class')
 nx.set_node_attributes(graph, gender, 'gender')
 
-print(graph.nodes(data=True))
-
 # Prepare csv for dataframe
 dest_file = os.path.join(primaryschool_dataset_dir, 'prepared_data.csv')
-with open(primaryschool['dataset'], 'r') as source:
-    reader = csv.reader(source, delimiter='\t')
-    with open(dest_file, 'w') as result:
-        writer = csv.writer(result, delimiter='\t')
-        writer.writerow(
-            ('class1', 'gender1', 'class2', 'gender2')
-        )
-        for row in reader:
-            writer.writerow(
-                (row[3], gender[int(row[1])], row[4], gender[int(row[2])])
-            )
-
+PrimarySchoolDatasetHandler.prepare_training_dataset(
+    primaryschool['dataset'], dest_file, gender)
 df = pd.read_csv(dest_file, sep='\t')
-print(df)
 
 # Load Workplace dataset
-# read metadata
-department = {}
-with open('Datasets/workplace/metadata_InVS13.txt', 'r') as f:
-    for line in f:
-        split = line.split()
-        department[int(split[0])] = split[1]
+# Read metadata
+department = WorkplaceDatasetHandler.read_metadata('Datasets/workplace/metadata_InVS13.txt')
 
-# create csv dataframe
+# Prepare csv for dataframe
 dest_file2 = 'Datasets/workplace/prepared_data.csv'
-with open('Datasets/workplace/tij_InVS.dat', 'r') as source:
-    reader = csv.reader(source, delimiter=' ')
-    with open(dest_file2, 'w') as result:
-        writer = csv.writer(result, delimiter='\t')
-        writer.writerow(
-            ('department1', 'department2')
-        )
-        for row in reader:
-            writer.writerow(
-                (department[int(row[1])], department[int(row[2])])
-            )
+WorkplaceDatasetHandler.prepare_training_dataset(
+    'Datasets/workplace/tij_InVS.dat', dest_file2, department)
 df2 = pd.read_csv(dest_file2, sep='\t')
-print(df2)
+
 # Transform network to training dataset
 
 # Priority Rank
--- utils.py
+++ utils.py
@@ -35,3 +35,47 @@ class PrimarySchoolDatasetHandler:
                 class_id[int(split[0])] = split[1]
                 gender[int(split[0])] = split[2]
         return class_id, gender
+
+    @staticmethod
+    def prepare_training_dataset(dataset_file, dest_file, gender):
+        with open(dataset_file, 'r') as source:
+            reader = csv.reader(source, delimiter='\t')
+            with open(dest_file, 'w') as result:
+                writer = csv.writer(result, delimiter='\t')
+                writer.writerow(
+                    ('class1', 'gender1', 'class2', 'gender2')
+                )
+                for row in reader:
+                    writer.writerow(
+                        (row[3], gender[int(row[1])], row[4],
+                         gender[int(row[2])])
+                    )
+
+
+class WorkplaceDatasetHandler:
+    """
+
+    """
+
+    @staticmethod
+    def read_metadata(metadata_file):
+        department = {}
+        with open(metadata_file, 'r') as f:
+            for line in f:
+                split = line.split()
+                department[int(split[0])] = split[1]
+        return department
+
+    @staticmethod
+    def prepare_training_dataset(dataset_file, dest_file, department):
+        with open(dataset_file, 'r') as source:
+            reader = csv.reader(source, delimiter=' ')
+            with open(dest_file, 'w') as result:
+                writer = csv.writer(result, delimiter='\t')
+                writer.writerow(
+                    ('department1', 'department2')
+                )
+                for row in reader:
+                    writer.writerow(
+                        (department[int(row[1])], department[int(row[2])])
+                    )
