8e98772f4797c4b32cc5e00d6b8789c0f6d46aec
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2017-12-20 12:59:20 +0100

    Adjust graph to metadata changes

--- graph.py
+++ graph.py
@@ -8,9 +8,10 @@ from config import primaryschool
 if not os.path.exists(primaryschool['prepared_graph_dataset']):
     PrimarySchoolDatasetHandler.prepare_graph_dataset(primaryschool['dataset'], primaryschool['prepared_graph_dataset'])
 # Read metadata
-class_id, gender = PrimarySchoolDatasetHandler.read_metadata(primaryschool['metadata'])
+attributes = PrimarySchoolDatasetHandler.read_metadata(primaryschool['metadata'])
 # Create graph with edges loaded from dataset
 graph = nx.read_edgelist(primaryschool['prepared_graph_dataset'], nodetype=int)
+# TODO: update and fix
 # Add attributes to nodes
-nx.set_node_attributes(graph, class_id, 'class')
-nx.set_node_attributes(graph, gender, 'gender')
+# nx.set_node_attributes(graph, class_id, 'class')
+# nx.set_node_attributes(graph, gender, 'gender')
