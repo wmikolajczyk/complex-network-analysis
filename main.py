660b58b5fa838fb98e74e85c10dc2529a1f893fb
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-01-31 22:24:49 +0100

    Remove priority_rank from main

--- main.py
+++ main.py
@@ -36,45 +36,6 @@ def main():
 
     primaryschool_df = pd.read_csv(primaryschool['prepared_dataset'], sep='\t')
 
-    # Priority Rank
-    new_graph = nx.MultiGraph()
-    num_of_edges = 3
-    len_of_ranking = 5
-
-    df = primaryschool_df
-    # TODO: fix gender Unknown values
-
-    node_number = 0
-    # Iterate over existing nodes
-    for node in nodes_list:
-        # Extract node attributes
-        attributes = node_attributes[node]
-        node_number += 1
-        # Get base node number for creating edge
-        base_node_number = node_number
-        # Add base node to the new graph
-        new_graph.add_node(node_number, **attributes)
-        # Compute ranking based on vertex attributes
-        ranking = df[(df['class1']==attributes['class']) & (df['gender1']==attributes['gender'])]\
-            .groupby(['class1', 'gender1', 'class2', 'gender2'], as_index=False)[['num_of_connections']]\
-            .sum()\
-            .sort_values(['num_of_connections'], ascending=False)\
-            .head(len_of_ranking)
-        # Add k number of edges
-        for k in range(0, num_of_edges):
-            # Sample vertex t from the ranking
-            ranking_idx = np.random.choice(len(ranking))
-            # Get new node attributes dict
-            new_attributes = ranking.iloc[[ranking_idx]][['class2', 'gender2']]\
-                .rename(columns={'class2': 'class', 'gender2': 'gender'})\
-                .to_dict(orient='records')[0]
-            node_number += 1
-            # Add new node to the new graph
-            new_graph.add_node(node_number, **new_attributes)
-            # Add edge between base and new node
-            new_graph.add_edge(base_node_number, node_number)
-
-
     # Experimental simple model
     # Clean data, add dummy columns
     primaryschool_df = PrimarySchoolDatasetHandler.clean_data(primaryschool_df)
