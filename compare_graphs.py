492e881a6fa1942e4a007a5aa2f095a757c1d2c2
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-02-06 13:23:17 +0100

    Add compare graphs function

---
+++ compare_graphs.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8
+import networkx as nx
+
+from collections import OrderedDict
+
+from scipy import stats
+
+
+def compare(graph1, graph2):
+    """
+    TODO: speed up - why it takes so much time?
+    Compare two graphs
+
+    Args:
+        graph1: First graph to compare
+        graph2: Second graph to compare
+
+    Returns:
+        A dict with calculated diffrence measures
+    """
+    result = OrderedDict()
+    # KS test for graph measurements distributions
+    graph1_degree_centrality = list(nx.degree_centrality(graph1).values())
+    graph2_degree_centrality = list(nx.degree_centrality(graph2).values())
+    result['degree_centrality'] = stats.ks_2samp(graph1_degree_centrality,
+                                                 graph2_degree_centrality)
+
+    graph1_closeness_centrality = list(
+        nx.closeness_centrality(graph1).values())
+    graph2_closeness_centrality = list(
+        nx.closeness_centrality(graph2).values())
+    result['closeness_centrality'] = stats.ks_2samp(
+        graph1_closeness_centrality, graph2_closeness_centrality)
+
+    graph1_betweenness_centrality = list(
+        nx.betweenness_centrality(graph1).values())
+    graph2_betweenness_centrality = list(
+        nx.betweenness_centrality(graph2).values())
+    result['betweenness_centrality'] = stats.ks_2samp(
+        graph1_betweenness_centrality, graph2_betweenness_centrality)
+
+    graph1_pagerank = list(nx.pagerank(graph1).values())
+    graph2_pagerank = list(nx.pagerank(graph2).values())
+    result['pagerank'] = stats.ks_2samp(graph1_pagerank, graph2_pagerank)
+
+    # absolute value of global graph measurements subtraction
+    result['average_shortest_path_length'] = abs(
+        nx.average_shortest_path_length(
+            graph1) - nx.average_shortest_path_length(graph2))
+    result['diameter'] = abs(nx.diameter(graph1) - nx.diameter(graph2))
+    result['degree_centrailty'] = '?'
+    result['density'] = abs(nx.density(graph1) - nx.density(graph2))
+
+    return result
+
+if __name__ == '__main__':
+    # Example usage of compare function
+    n1, p1 = 100, 0.8
+    n2, p2 = 100, 0.7
+
+    graph1 = nx.gnp_random_graph(n1, p1, seed=93)
+    graph2 = nx.gnp_random_graph(n2, p2, seed=95)
+
+    print(compare(graph1, graph2))
