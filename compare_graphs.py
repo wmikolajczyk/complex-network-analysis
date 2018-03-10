aae4cee80320407247a07e95f6a6078dfc8f7a7e
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-03-10 12:08:39 +0100

    Add _delta to comparsion dict result keys

--- compare_graphs.py
+++ compare_graphs.py
@@ -21,37 +21,37 @@ def compare(graph1, graph2):
     # KS test for graph measurements distributions
     graph1_degree_centrality = list(nx.degree_centrality(graph1).values())
     graph2_degree_centrality = list(nx.degree_centrality(graph2).values())
-    result['degree_centrality'] = stats.ks_2samp(graph1_degree_centrality,
+    result['degree_centrality_delta'] = stats.ks_2samp(graph1_degree_centrality,
                                                  graph2_degree_centrality)
 
     graph1_closeness_centrality = list(
         nx.closeness_centrality(graph1).values())
     graph2_closeness_centrality = list(
         nx.closeness_centrality(graph2).values())
-    result['closeness_centrality'] = stats.ks_2samp(
+    result['closeness_centrality_delta'] = stats.ks_2samp(
         graph1_closeness_centrality, graph2_closeness_centrality)
 
     graph1_betweenness_centrality = list(
         nx.betweenness_centrality(graph1).values())
     graph2_betweenness_centrality = list(
         nx.betweenness_centrality(graph2).values())
-    result['betweenness_centrality'] = stats.ks_2samp(
+    result['betweenness_centrality_delta'] = stats.ks_2samp(
         graph1_betweenness_centrality, graph2_betweenness_centrality)
 
     graph1_pagerank = list(nx.pagerank(graph1).values())
     graph2_pagerank = list(nx.pagerank(graph2).values())
-    result['pagerank'] = stats.ks_2samp(graph1_pagerank, graph2_pagerank)
+    result['pagerank_delta'] = stats.ks_2samp(graph1_pagerank, graph2_pagerank)
 
     # absolute value of global graph measurements subtraction
     try:
-        result['average_shortest_path_length'] = abs(
+        result['average_shortest_path_length_delta'] = abs(
             nx.average_shortest_path_length(
                 graph1) - nx.average_shortest_path_length(graph2))
     except nx.NetworkXError as e:
         # graph is not connected - it's possible for Watts-Strogats
         print('Cannot compute average_shortest_path_length - {}'.format(e))
     try:
-        result['diameter'] = abs(nx.diameter(graph1) - nx.diameter(graph2))
+        result['diameter_delta'] = abs(nx.diameter(graph1) - nx.diameter(graph2))
     except nx.NetworkXError as e:
         # graph is not connected - it's possible for Watts-Strogats
         print('Cannot compute diameter - {}'.format(e))
@@ -63,9 +63,9 @@ def compare(graph1, graph2):
     graph2_degree_deltas = [graph2_max_degree - v for k, v in graph2.degree]
     graph2_degree_centralization = sum(graph2_degree_deltas) / max(
         graph2_degree_deltas)
-    result['degree_centralization'] = abs(
+    result['degree_centralization_delta'] = abs(
         graph1_degree_centralization - graph2_degree_centralization)
-    result['density'] = abs(nx.density(graph1) - nx.density(graph2))
+    result['density_delta'] = abs(nx.density(graph1) - nx.density(graph2))
 
     return result
 
