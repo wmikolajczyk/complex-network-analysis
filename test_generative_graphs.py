216dd4b0f433dd42fc3e648e0db3d6daa0e02609
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-03-27 22:54:18 +0200

    Add parametrized graphs

--- test_generative_graphs.py
+++ test_generative_graphs.py
@@ -15,21 +15,43 @@ def test_generative_graph(graph_func, *args, **kwargs):
 	model = get_trained_model(graph)
 	new_graph = generate_graph_by_nn(model, graph, avg_num_edges)
 	# GET COMPARSION DICT FOR ORIGINAL AND GENERATED GRAPHS
-	comparsion_dict = compare(graph, new_graph) 
+	comparsion_dict = compare(graph, new_graph)
 	# PRINT ORIGINAL AND GENERATED GRAPH COMPARSION RESULTS
 	print('Graph type: {type}'.format(type=graph_func.__name__))
 	[print('{}: {}'.format(k, v)) for k, v in comparsion_dict.items()]
 
 	if kwargs.get('show_graph'):
 		# DRAW ORIGINAL GRAPH
+		plt.figure(1)
 		nx.draw(graph, with_labels=True)
-		plt.show()
 		# DRAW GENERATED GRAPH
+		plt.figure(2)
 		nx.draw(new_graph, with_labels=True)
 		plt.show()
 
+# Probability of connection between nodes
+erdos_renyi_params = [
+	{'n': 30, 'p': 0.05},
+	{'n': 30, 'p': 0.1},
+	{'n': 30, 'p': 0.3}
+]
+for param in erdos_renyi_params:
+	test_generative_graph(nx.erdos_renyi_graph, param['n'], param['p'], show_graph=True)
 
-test_generative_graph(nx.erdos_renyi_graph, 30, 0.6)
-test_generative_graph(nx.watts_strogatz_graph, 30, 2, 0.6)
-test_generative_graph(nx.barabasi_albert_graph, 30, 2, show_graph=True)
+# Probability of
+watts_strogatz_params = [
+	{'n': 30, 'k': 2, 'p': 0.05},
+	{'n': 30, 'k': 2, 'p': 0.1},
+	{'n': 30, 'k': 2, 'p': 0.3}
+],
+for param in watts_strogatz_params:
+	test_generative_graph(nx.watts_strogatz_graph, param['n'], param['k'], param['p'], show_graph=True)
+
+barabasi_albert_params = [
+	{'n': 30, 'm': 1},
+	{'n': 30, 'm': 2},
+	{'n': 30, 'm': 5}
+]
+for param in barabasi_albert_params:
+	test_generative_graph(nx.barabasi_albert_graph, param['n'], param['m'], show_graph=True)
 
