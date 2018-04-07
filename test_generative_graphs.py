b6d587bec3e863f31e18c02a913f9a34e0da70ca
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-04-07 23:54:01 +0200

    Refactor graph comparsion functions

--- test_generative_graphs.py
+++ test_generative_graphs.py
@@ -4,27 +4,30 @@ import matplotlib.pyplot as plt
 from compare_graphs import compare
 from generate_graph_nn import attach_attributes, get_trained_model, generate_graph_by_nn
 
-def test_generative_graph(graph_func, *args, **kwargs):
+
+def generate_graph(graph_func, params):
+    # set seed
     seed = 93
-
-    # GENERATE GRAPH, CALCULATE NODE ATTRIBUTES AND ATTACH THESE ATTRIBUTES
-    graph = graph_func(*args, seed=seed)
+    # generate graph by passed function and args
+    graph = graph_func(**params, seed=seed)
+    # calculate and attach node attributes
     attach_attributes(graph)
-    # CALCULATE AVG NUM EDGES FOR PRIORITY RANK, TRAIN MODEL, GENERATE NEW GRAPH BY NN USING TRAINED MODEL
-    avg_num_edges = round(graph.number_of_edges() / graph.number_of_nodes())
-    model = get_trained_model(graph)
-    new_graph = generate_graph_by_nn(model, graph, avg_num_edges)
-    # GET COMPARSION DICT FOR ORIGINAL AND GENERATED GRAPHS
-    comparsion_dict = compare(graph, new_graph)
-    # PRINT ORIGINAL AND GENERATED GRAPH COMPARSION RESULTS
-    print('Graph type: {type}'.format(type=graph_func.__name__))
-    [print('{}: {}'.format(k, v)) for k, v in comparsion_dict.items()]
+    return graph
 
-    if kwargs.get('show_graph'):
-        # DRAW ORIGINAL GRAPH
-        plt.figure(1)
-        nx.draw(graph, with_labels=True)
-        # DRAW GENERATED GRAPH
-        plt.figure(2)
-        nx.draw(new_graph, with_labels=True)
-        plt.show()
+def recreate_graph(graph):
+    # calculate average number of edges in original graph
+    avg_num_edges = round(graph.number_of_edges() / graph.number_of_nodes())
+    # train a model
+    model = get_trained_model(graph)
+    # generate new graph using trained model
+    new_graph = generate_graph_by_nn(model, graph, avg_num_edges)
+    return new_graph
+
+def print_comparsion_result(comparsion_dict):
+    for key, val in comparsion_dict.items():
+        print('{}: {}'.format(key, val))
+
+
+def draw_graph(graph):
+    nx.draw(graph, with_labels=True)
+    plt.show()
