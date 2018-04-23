6ff9e98788e1f6fff533e9f7eb23dbc8a96a03dd
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-04-23 14:34:49 +0200

    Comment some prints

--- load_graph.py
+++ load_graph.py
@@ -123,14 +123,14 @@ def load_graph(extracted_filepath):
         return
     adj_file = out_files[0]
 
-    print('Loading graph...')
+    # print('Loading graph...')
     graph = nx.read_adjlist(os.path.join(extracted_filepath, adj_file), comments='%')
     graph = nx.convert_node_labels_to_integers(graph)
 
     # print('Attaching attributes (graph measurements)...')
     # attach_attributes(graph)
 
-    print('Attaching real attributes...')
+    # print('Attaching real attributes...')
 
     ent_gens = []
     for ent in ent_files:
@@ -177,6 +177,6 @@ for dataset_name in available_datasets:
         print(dataset_name)
         checked_dataset_with_attrs[num_of_attrs].append(dataset_name)
 
-    print(checked_dataset_with_attrs)
+    # print(checked_dataset_with_attrs)
     with open('best_datasets.pkl', 'wb') as file:
         pickle.dump(checked_dataset_with_attrs, file)
