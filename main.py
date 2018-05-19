6f531171f6de0b4184a4b35a76325fd5e63e7fdd
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-05-19 14:21:27 +0200

    Update main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import networkx as nx
 import pandas as pd
 
 from graph import generate_graph, attach_graph_attributes, \
-    get_graph_measurements, compare_graph_measurements
+    get_graph_measurements, compare_graph_measurements, print_comparison_results
 
 from recreate_graph import graph_to_dataframe, get_trained_model, \
     recreate_by_priority_rank
@@ -11,9 +11,9 @@ from recreate_graph import graph_to_dataframe, get_trained_model, \
 def minMax(x):
     return pd.Series(index=['min','max'],data=[x.min(),x.max()])
 
-params = {'n': 30, 'm': 1}
+params = {'n': 50, 'k': 2, 'p': 0.15}
 
-graph = generate_graph(nx.barabasi_albert_graph, params)
+graph = generate_graph(nx.watts_strogatz_graph, params)
 attach_graph_attributes(graph)
 
 
@@ -26,5 +26,5 @@ new_graph = recreate_by_priority_rank(graph, df, model)
 graph_measurements = get_graph_measurements(graph)
 new_graph_measurements = get_graph_measurements(new_graph)
 x = compare_graph_measurements(graph_measurements, new_graph_measurements)
-print(x)
+print_comparison_results(x)
 #import pdb; pdb.set_trace()
