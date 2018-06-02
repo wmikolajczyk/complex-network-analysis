4a18ccbb00926314737b180b30f636f54e60b5fc
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-06-02 13:11:57 +0200

    Make load graph generic

--- real_graphs.py
+++ real_graphs.py
@@ -3,18 +3,21 @@ import os
 import networkx as nx
 import pandas as pd
 
+delimiter = '\t'
 
-def load_primary_school():
-    prepared_primary_school = 'prepared_datasets/primary_school'
-    prepared_edge_list = os.path.join(prepared_primary_school, 'edge_list.csv')
-    prepared_node_attributes = os.path.join(prepared_primary_school, 'node_attributes.csv')
+prepared_datsets_path = 'prepared_datasets'
+
+
+def load_dataset_to_graph(dataset_dir):
+    prepared_edge_list = os.path.join(dataset_dir, 'edge_list.csv')
+    prepared_node_attributes = os.path.join(dataset_dir, 'node_attributes.csv')
 
     #       LOAD EDGES
     graph = nx.read_edgelist(prepared_edge_list, create_using=nx.DiGraph(), nodetype=int)
 
     #       LOAD ATTRIBUTES
-    attributes_data = pd.read_csv(prepared_node_attributes, delimiter='\t', header=None)
-    attributes_data.columns = ['node_id', 'class', 'gender']
+    attributes_data = pd.read_csv(prepared_node_attributes, delimiter=delimiter)
+    # list of node attributes without node_id
     attributes_columns = list(attributes_data.columns)
     attributes_columns.remove('node_id')
     for node_id in graph.nodes:
@@ -26,6 +29,13 @@ def load_primary_school():
         graph.node[node_id].update(node_attributes)
     return graph
 
-g = load_primary_school()
+primary_school_path = os.path.join(prepared_datsets_path, 'primary_school')
+g = load_dataset_to_graph(primary_school_path)
 print('done')
 import pdb; pdb.set_trace()
+
+# Graph to dataframe
+
+# get trained model
+
+# recreate by priority rank
