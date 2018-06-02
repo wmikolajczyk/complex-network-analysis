2c5077906d51a989cacc6d4792d5bdb58e8007cc
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-06-02 15:32:24 +0200

    Add workplace dataset preprocess wrapper

--- dataset_wrappers.py
+++ dataset_wrappers.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import shutil
 import json
 
 import networkx as nx
@@ -63,12 +62,47 @@ def prepare_primary_school(dataset_name):
     nx.write_edgelist(directed_weighted_graph, prepared_edge_list, delimiter=delimiter)
 
     #           PROCESS ATTRIBUTES
-    shutil.copy(node_attributes, prepared_node_attributes)
     attrs_df = pd.read_csv(node_attributes, delimiter=delimiter, 
         names=['node_id', 'class', 'gender'])
-    #import pdb; pdb.set_trace()
     attrs_df.to_csv(prepared_node_attributes, sep=delimiter, index=False)
 
 
-prepare_primary_school('primary_school')
+# prepare_primary_school('primary_school')
+
+def prepare_workplace(dataset_name):
+    raw_workplace = os.path.join(raw_datasets_path, dataset_name)
+    edge_list = os.path.join(raw_workplace, 'tij_InVS.dat')
+    node_attributes = os.path.join(raw_workplace, 'metadata_InVS13.txt')
+
+    prepared_workplace = os.path.join(prepared_datsets_path, dataset_name)
+    prepared_edge_list = os.path.join(prepared_workplace, 'edge_list.csv')
+    prepared_node_attributes = os.path.join(prepared_workplace, 'node_attributes.csv')
+
+    if not os.path.exists(prepared_workplace):
+        os.mkdir(prepared_workplace)
+
+    #           PROCESS EDGES
+    with open(edge_list, 'r') as source:
+        reader = csv.reader(source, delimiter=' ')
+        with open(prepared_edge_list, 'w') as result:
+            writer = csv.writer(result, delimiter=' ')
+            for row in reader:
+                writer.writerow((row[1], row[2]))
+    #               Convert multiple edges to weights
+    weighted_graph = nx.Graph()
+    multi_graph = nx.read_edgelist(prepared_edge_list, create_using=nx.MultiGraph())
+
+    for u, v, data in multi_graph.edges(data=True):
+        if weighted_graph.has_edge(u, v):
+            weighted_graph[u][v]['weight'] += 1
+        else:
+            weighted_graph.add_edge(u, v, weight=1)
+    #               Convert graph to directed
+    directed_weighted_graph = weighted_graph.to_directed()
+    nx.write_edgelist(directed_weighted_graph, prepared_edge_list, delimiter=delimiter)
+    #           PROCESS ATTRIBUTES
+    attrs_df = pd.read_csv(node_attributes, delimiter='\t', names=['node_id', 'department'])
+    attrs_df.to_csv(prepared_node_attributes, sep=delimiter, index=False)
+
+prepare_workplace('workplace')
 print('done')
