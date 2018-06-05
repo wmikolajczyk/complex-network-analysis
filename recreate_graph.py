d2503a43e37c748c0fa10bc3505435cb4ea630c2
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-06-05 18:27:51 +0200

    Split graph to dataframe to creating df and preprocessing

--- recreate_graph.py
+++ recreate_graph.py
@@ -10,16 +10,6 @@ from sklearn import preprocessing
 from tensorflow import set_random_seed
 
 
-def recreate_graph(graph):
-    # build df form graph
-    df = graph_to_dataframe(graph)
-    # train a model
-    model = get_trained_model(df)
-    # generate new graph using trained model
-    new_graph = recreate_by_priority_rank(graph, df, model)
-    return new_graph
-
-
 def get_trained_model(df, epochs=128, batch_size=64, verbose=1):
     # number of attributes without target
     number_of_attrs = len(df.columns) - 1
@@ -77,27 +67,32 @@ def graph_to_dataframe(graph):
             row['num_of_edges'] = weight
             rows.append(row)
     df = pd.DataFrame(rows)
-    # TODO: think about that
+    return df
+
+
+def preprocess_dataframe(df, number_of_nodes):
     # all attributes are strings (object type)
     # try to convert them to numeric (ignore errors - string which cannot be converted)
     df = df.apply(pd.to_numeric, errors='ignore')
     # handle categorical columns (str)
     coltypes_dict = dict(df.dtypes)
+    # get string columns
     str_columns = [key for key in coltypes_dict
                    if coltypes_dict[key] == 'object']
-    # cast coltype to category
+    # set string columns to category
     for column in str_columns:
         df[column] = df[column].astype('category')
     # drop column if number of unique values = number of nodes
     #   because it means this column is like string id for node
     for column in df.select_dtypes(['category']).columns:
-        if df[column].nunique() == graph.number_of_nodes():
+        if df[column].nunique() == number_of_nodes:  # graph.number_of_nodes():
             df.drop(column, axis=1, inplace=True)
             print('dropped column {}'.format(column))
+    # get dummies for categorical variable
     df = pd.get_dummies(df, columns=df.select_dtypes(['category']).columns)
     # reorder columns to ensure that target column is last
-    # colnames are like node1_attrname, node2_attrname, num_of_conn
-    # so alphabetical order is correct
+    #   colnames are like node1_attrname, node2_attrname, num_of_conn
+    #   so alphabetical order is correct
     df = df.reindex(sorted(df.columns), axis=1)
 
     # minmax scaler - normalize values
@@ -105,7 +100,6 @@ def graph_to_dataframe(graph):
     scaled_values = min_max_scaler.fit_transform(df)
     df.loc[:, :] = scaled_values
     return df
-
 
 def recreate_by_priority_rank(graph, df, model):
     num_edges = round(graph.number_of_edges() / graph.number_of_nodes())
