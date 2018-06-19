9b4be223f3afb4d666bcae403574d56bcd77d8f7
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-06-19 12:11:13 +0200

    Add test_ks_passed parameter in measurements, remove old comparison methods, update notebooks

--- measurements.py
+++ measurements.py
@@ -105,6 +105,7 @@ def collect_graph_measurements(graph1_measurements, graph2_measurements):
 
 def get_measurements_results_df(df, orig_df):
     alpha = 0.95
+    pval_threshold = 0.05
     results = []
     for measure in df.columns:
         # drop NaNs
@@ -125,38 +126,22 @@ def get_measurements_results_df(df, orig_df):
             'lower_endpoint': _lower, 
             'upper_endpoint': _upper, 
             'original': None,
-            'is_between_bounds': None
+            'is_between_bounds': None,
+            'ks_test_passed': None
         }
         if measure in orig_df.columns:
             row['original'] = orig_df[measure][0]
             if not np.isnan(row['original']):
                 row['is_between_bounds'] = bool(_lower <= orig_df[measure][0] <= _upper)
+
+        if '_ks_pval' in measure:
+            row['ks_test_passed'] = bool(mean > pval_threshold)
         results.append(row)
     results_df = pd.DataFrame(results)
     results_df = results_df.reindex(columns=[
         'measure', 'mean', 'lower_endpoint', 'upper_endpoint', 'original', 
-        'is_between_bounds'])
+        'is_between_bounds', 'ks_test_passed'])
     return results_df
-
-
-def compare_graph_measurements(graph1_measurements, graph2_measurements):
-    results = OrderedDict()
-    for measurement, m_type in MEASUREMENTS.items():
-        if m_type == 'list':
-            val = stats.ks_2samp(
-                graph1_measurements[measurement],
-                graph2_measurements[measurement]).pvalue
-        else:
-            if (graph1_measurements[measurement] is None or
-                graph2_measurements[measurement] is None) or graph1_measurements[measurement] == 0:
-                val = None
-            else:
-                # abs in denominator is required because some measures can have values < 0
-                val = abs(
-                    graph1_measurements[measurement] -
-                    graph2_measurements[measurement]) / abs(graph1_measurements[measurement])
-        results[measurement] = val
-    return results
 
 
 def print_graph_measurements(graph_measurements):
@@ -167,40 +152,3 @@ def print_graph_measurements(graph_measurements):
             continue
         msg += '  {}: {}\n'.format(measurement, graph_measurements[measurement])
     print(msg)
-
-
-def print_comparison_results(comparison_results):
-    ks_test_min_threshold = 0.05
-    abs_dist_max_threshold = 0.10
-
-    for measurement, m_type in MEASUREMENTS.items():
-        result = '{}: {}'.format(measurement, comparison_results[measurement])
-        if m_type == 'list':
-            name = '(KS test p-value)'
-            passed_test = comparison_results[measurement] > ks_test_min_threshold
-        else:
-            name = '(abs distance)'
-            if comparison_results[measurement] is not None:
-                passed_test = comparison_results[measurement] < abs_dist_max_threshold
-            else:
-                passed_test = None
-        msg = '{:18} {:50} [passed: {}]'.format(name, result, passed_test)
-        print(msg)
-
-
-def average_comparison(comparison_list):
-    keys = comparison_list[0].keys()
-    avg_dict = defaultdict(list)
-
-    for comparison in comparison_list:
-        for key in keys:
-            avg_dict[key].append(comparison[key])
-
-    for key in keys:
-        avg_dict[key] = list(filter(None, avg_dict[key]))
-        try:
-            avg_dict[key] = sum(avg_dict[key]) / len(avg_dict[key])
-        except ZeroDivisionError:
-            avg_dict[key] = None
-
-    return avg_dict
